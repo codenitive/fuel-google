bf6bf11d0a56b9ef19114c79353232852b58885b
Chase Hutchins <syntaqx@gmail.com>
2011-07-12 13:45:42 -0600

    Adding missing SERP class.

---
+++ classes/serp.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2011 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * FuelPHP Google package implementation. This namespace controls all Google
+ * package functionality, including multiple sub-namespaces for the various
+ * tools.
+ *
+ * @author     Chase "Syntaqx" Hutchins
+ * @version    1.0
+ * @package    Fuel
+ * @subpackage Google
+ */
+namespace Google;
+
+/**
+ * Enables the ability to track Search Engine Ranking Positions, namely the
+ * page and position of keywords.
+ *
+ * @author   Chase "Syntaqx" Hutchins
+ * @version  1.0
+ * @package  Fuel
+ * @package  Google
+ * @category classes
+ */
+class Serp {
+
+}
+
+/* End of file serp.php */
